0691f81399c1e5766016b7493c8cf0aafd4b986b
Daniel Nylander <github@danielnylander.se>
2026-09-21 18:19:57 +0200

    Prepare WiFi Analyzer 0.1.6

--- src/wifi_analyzer/app.py
+++ src/wifi_analyzer/app.py
@@ -674,7 +674,7 @@ class WifiAnalyzerWindow(Adw.ApplicationWindow):
         about = Adw.AboutDialog(
             application_name="WiFi Analyzer",
             application_icon=APP_ID,
-            version="0.1.5",
+            version="0.1.6",
             developer_name="Daniel Nylander",
             license_type=Gtk.License.GPL_3_0,
             website="https://github.com/yeager/wifi-analyzer",
